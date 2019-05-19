b5ba44ea02ff568934757b561440c2338a72fe31
sd517125 <sd517125@163.com>
2019-05-20 00:45:53 +0800

    tianmao-1

---
+++ js/loginNavigationTop.js
@@ -0,0 +1,171 @@
+window.onload = function () {
+  getElementById("navigation-top-link2-img").onmouseover = function () {
+    getElementById("navigation-top-link2-QRcode").style.display = "block"
+  }
+  getElementById("navigation-top-link2-img").onmouseout = function () {
+    getElementById("navigation-top-link2-QRcode").style.display = "none";
+  }
+  getElementById("navigation-top-in-fight-mytaobao").onmouseover = function () {
+    getElementById("navigation-top-in-fight-mytaobao-lists").style.display = "block";
+  }
+  getElementById("navigation-top-in-fight-mytaobao").onmouseout = function () {
+    getElementById("navigation-top-in-fight-mytaobao-lists").style.display = "none";
+  }
+  getElementById("navigation-top-in-fight-collect").onmouseover = function () {
+    getElementById("navigation-top-in-fight-collect-lists").style.display = "block";
+  }
+  getElementById("navigation-top-in-fight-collect").onmouseout = function () {
+    getElementById("navigation-top-in-fight-collect-lists").style.display = "none";
+  }
+  getElementById("navigation-top-in-fight-merchant-list").onmouseover = function () {
+    getElementById("navigation-top-in-fight-merchant-lists").style.display = "block";
+  }
+  getElementById("navigation-top-in-fight-merchant-list").onmouseout = function () {
+    getElementById("navigation-top-in-fight-merchant-lists").style.display = "none";
+  }
+  getElementById("navigation-top-in-fight-frame-list").onmouseover = function () {
+    getElementById("navigation-top-in-fight-frame-lists").style.display = "block";
+  }
+  getElementById("navigation-top-in-fight-frame-list").onmouseout = function () {
+    getElementById("navigation-top-in-fight-frame-lists").style.display = "none";
+  }
+  getElementById("navigation-header-classify").onmouseover = function () {
+    getElementById("navigation-header-classify").style.backgroundColor="#ddd";
+    getElementById("navigation-header-classify").style.color="white";
+    getElementById("navigation-header-activity").style.backgroundColor="red";
+    getElementById("navigation-header-activity").style.color="orange";
+    getElementById("navigation-header-activity-list").style.display = "none";
+    getElementById("navigation-header-classify-list").style.display = "block";
+  }
+  getElementById("navigation-header-activity").onmouseover = function () {
+    getElementById("navigation-header-classify").style.backgroundColor="red";
+    getElementById("navigation-header-classify").style.color="white";
+    getElementById("navigation-header-activity").style.backgroundColor="#444";
+    getElementById("navigation-header-activity").style.color="white";
+    getElementById("navigation-header-classify-list").style.display = "none";
+    getElementById("navigation-header-activity-list").style.display = "block";
+  }
+  getElementById("navigation-header-classify-list-1").onmouseover = function () {
+    getElementById("navigation-header-classify-list-1-out").style.display = "block";
+  }
+  getElementById("navigation-header-classify-list-1").onmouseout = function () {
+    getElementById("navigation-header-classify-list-1-out").style.display = "none";
+  }
+  getElementById("navigation-header-classify-list-2").onmouseover = function () {
+    getElementById("navigation-header-classify-list-2-out").style.display = "block";
+  }
+  getElementById("navigation-header-classify-list-2").onmouseout = function () {
+    getElementById("navigation-header-classify-list-2-out").style.display = "none";
+  }
+  getElementById("navigation-header-classify-list-3").onmouseover = function () {
+    getElementById("navigation-header-classify-list-3-out").style.display = "block";
+  }
+  getElementById("navigation-header-classify-list-3").onmouseout = function () {
+    getElementById("navigation-header-classify-list-3-out").style.display = "none";
+  }
+  getElementById("navigation-header-classify-list-4").onmouseover = function () {
+    getElementById("navigation-header-classify-list-4-out").style.display = "block";
+  }
+  getElementById("navigation-header-classify-list-4").onmouseout = function () {
+    getElementById("navigation-header-classify-list-4-out").style.display = "none";
+  }
+  getElementById("navigation-header-classify-list-5").onmouseover = function () {
+    getElementById("navigation-header-classify-list-5-out").style.display = "block";
+  }
+  getElementById("navigation-header-classify-list-5").onmouseout = function () {
+    getElementById("navigation-header-classify-list-5-out").style.display = "none";
+  }
+  getElementById("navigation-header-classify-list-6").onmouseover = function () {
+    getElementById("navigation-header-classify-list-6-out").style.display = "block";
+  }
+  getElementById("navigation-header-classify-list-6").onmouseout = function () {
+    getElementById("navigation-header-classify-list-6-out").style.display = "none";
+  }
+  getElementById("navigation-header-classify-list-7").onmouseover = function () {
+    getElementById("navigation-header-classify-list-7-out").style.display = "block";
+  }
+  getElementById("navigation-header-classify-list-7").onmouseout = function () {
+    getElementById("navigation-header-classify-list-7-out").style.display = "none";
+  }
+  getElementById("navigation-header-classify-list-8").onmouseover = function () {
+    getElementById("navigation-header-classify-list-8-out").style.display = "block";
+  }
+  getElementById("navigation-header-classify-list-8").onmouseout = function () {
+    getElementById("navigation-header-classify-list-8-out").style.display = "none";
+  }
+  getElementById("navigation-header-classify-list-9").onmouseover = function () {
+    getElementById("navigation-header-classify-list-9-out").style.display = "block";
+  }
+  getElementById("navigation-header-classify-list-9").onmouseout = function () {
+    getElementById("navigation-header-classify-list-9-out").style.display = "none";
+  }
+  getElementById("navigation-header-classify-list-10").onmouseover = function () {
+    getElementById("navigation-header-classify-list-10-out").style.display = "block";
+  }
+  getElementById("navigation-header-classify-list-10").onmouseout = function () {
+    getElementById("navigation-header-classify-list-10-out").style.display = "none";
+  }
+  getElementById("navigation-header-classify-list-11").onmouseover = function () {
+    getElementById("navigation-header-classify-list-11-out").style.display = "block";
+  }
+  getElementById("navigation-header-classify-list-11").onmouseout = function () {
+    getElementById("navigation-header-classify-list-11-out").style.display = "none";
+  }
+  getElementById("navigation-header-classify-list-12").onmouseover = function () {
+    getElementById("navigation-header-classify-list-12-out").style.display = "block";
+  }
+  getElementById("navigation-header-classify-list-12").onmouseout = function () {
+    getElementById("navigation-header-classify-list-12-out").style.display = "none";
+  }
+  getElementById("navigation-header-classify-list-13").onmouseover = function () {
+    getElementById("navigation-header-classify-list-13-out").style.display = "block";
+  }
+  getElementById("navigation-header-classify-list-13").onmouseout = function () {
+    getElementById("navigation-header-classify-list-13-out").style.display = "none";
+  }
+  getElementById("navigation-header-classify-list-14").onmouseover = function () {
+    getElementById("navigation-header-classify-list-14-out").style.display = "block";
+  }
+  getElementById("navigation-header-classify-list-14").onmouseout = function () {
+    getElementById("navigation-header-classify-list-14-out").style.display = "none";
+  }
+  getElementById("navigation-header-classify-list-15").onmouseover = function () {
+    getElementById("navigation-header-classify-list-15-out").style.display = "block";
+  }
+  getElementById("navigation-header-classify-list-15").onmouseout = function () {
+    getElementById("navigation-header-classify-list-15-out").style.display = "none";
+  }
+  getElementById("navigation-header-classify-list-16").onmouseover = function () {
+    getElementById("navigation-header-classify-list-16-out").style.display = "block";
+  }
+  getElementById("navigation-header-classify-list-16").onmouseout = function () {
+    getElementById("navigation-header-classify-list-16-out").style.display = "none";
+  }
+
+  lunbo();
+}
+function getElementById(id) {
+  return document.getElementById(id);
+}
+function getElementsByClassName(className) {
+  return document.getElementsByClassName(className);
+}
+/*轮播*/
+function lunbo() {
+  var nacigationHeaderBottomImgs = getElementsByClassName("nacigation-header-bottom-img");
+  var nacigationHeaderBottomIndexs = getElementsByClassName("nacigation-header-bottom-index")
+  // alert(nacigationHeaderBottomImgs.length);
+  var count = 0;
+  setInterval(function () {
+    if (count == nacigationHeaderBottomIndexs.length) {
+      count = 0;
+    }
+    for (var i=0;i<nacigationHeaderBottomIndexs.length;i++) {
+      nacigationHeaderBottomImgs[i].style.display = "none";
+      nacigationHeaderBottomIndexs[i].style.backgroundColor="#888";
+    }
+    nacigationHeaderBottomImgs[count].style.display = "block";
+    nacigationHeaderBottomIndexs[count].style.backgroundColor = "#ddd";
+    count++;
+  },3000);
+}
